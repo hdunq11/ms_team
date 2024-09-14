64b87f1004d61fe9e480fd10afb629c8a5c1e8bd
hdunq11 <dunghoang080804@gmail.com>
2024-09-14 14:09:48 +0700

    add firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -21,26 +19,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export const auth = firebase.auth();
+export const db = firebase.firestore();
+export default firebase;
